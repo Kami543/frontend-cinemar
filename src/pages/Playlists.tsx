15039ca0764d6ac76889ff327d0e7365fbd7c68d
Kami543 <witennerrodrigues@gmail.com>
2026-05-31 20:28:59 -0300

    add imagens dos filmes

--- src/pages/Playlists.tsx
+++ src/pages/Playlists.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { FaTimes, FaSave, FaUpload, FaPlus, FaTrash } from 'react-icons/fa';
 import styles from '../styles/PlaylistForm.module.css';
-import { useTheme } from '../context/ThemeContext';
+import { useTheme } from '../components/context/ThemeContext';
 
 interface Playlist {
   id: string;
